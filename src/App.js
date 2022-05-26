6c3ce3b45d0ce56f0c78c4171428313592469fa0
Mareike <mareike.brandt@hotmail.de>
2022-05-26 14:02:22 +0900

    box 1: transition prop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Box1 from "./components/Box1/Box1";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Box1 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
